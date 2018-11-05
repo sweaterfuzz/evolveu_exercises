ba39de24368f08e33dd2139d06eafffd9dcea5fd
Julia Pulwicki <jpulwicki@gmail.com>
2018-11-05 13:09:33 -0700

    printing arrays and objects to the DOM

--- apiPractice/tying_it_all_together.js
+++ apiPractice/tying_it_all_together.js
@@ -30,9 +30,37 @@ function myPrint(someInfo) {
 
 myPrint('Hi from fake print');
 
+// take text from top div and move it to the bottom div
 document.querySelector('#moveToBottom').addEventListener("click", function(){
-    //document.getElementById("demo").innerHTML = "Hello World";
     var infoString = document.querySelector('#infoInTextArea').value;
     myPrint(infoString);
     document.querySelector('#bottomBox').textContent = infoString;
+});
+
+var playModule = function() {
+    console.log('play mod');
+}();
+
+document.querySelector('#jsonEvent').addEventListener("click", function(){
+    myPrint('json module stuff');
+});
+
+document.querySelector('#arrayPrint').addEventListener("click", function(){
+    var fibSeries = [1,1,2,3,5,8];
+    var fibString = ""
+    for (var i=0; i < fibSeries.length; i++){
+        fibString += "fibSeries " + i + "  " + fibSeries[i] + "\r\n";
+    }
+    document.getElementById('outputArea').setAttribute('style', 'white-space:pre;');
+    document.getElementById('outputArea').textContent = fibString;
+});
+
+document.querySelector('#objectPrint').addEventListener("click", function(){
+    var cityObject = {
+        name: 'Calgary',
+        population: 1200000,
+        province: 'Alberta',
+        hasMoutains: true,
+    };
+    document.getElementById('outputArea').textContent = JSON.stringify(cityObject);
 });
